712b2871182d650a12b9de4249824511966d659f
cmontes <cmontes@190.155.147.164>
2023-08-29 15:28:17 -0500

    avance en metodo iniciar sesion -Cecilia Montes

--- POO4_PROY2P_Montes_Reyes_Vilema/src/main/java/poo4_proy2p/IngresoSistema.java
+++ POO4_PROY2P_Montes_Reyes_Vilema/src/main/java/poo4_proy2p/IngresoSistema.java
@@ -5,54 +5,92 @@
 package poo4_proy2p;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.TextAlignment;
 
 /**
  *
  * @author cmontes
  */
 public class IngresoSistema implements Initializable {
-    
+
+    ArrayList<Usuario> usuarios = new ArrayList<>();
+
     @FXML
     private TextField tfusuario;
-    
+
     @FXML
     private ImageView imagenprincipal;
-    
+
     @FXML
     private Pane pane;
-    
+
     @FXML
     private HBox hDatos;
-    
+
     @FXML
     private VBox vLabels;
-    
+
     @FXML
     private VBox vDatos;
-    
+
     @FXML
     private TextField tfcontrasenia;
-    
+
     @FXML
     private Button btnIniciarSesion;
+
+    @FXML
+    private VBox vLabelError;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         //TO DO
     }
-    
+
     @FXML
-    public void iniciarSesion(){
-        Usuario u1 = new Usuario("majoabril", "majito1234");
+    public void iniciarSesion() {
+        Usuario u1 = new Usuario("majoab", "majito1234");
         Usuario u2 = new Usuario("cx", "c21X");
+        Usuario u3 = new Usuario("cmm", "12345");
+        Usuario u4 = new Usuario("amp", "abcd");
+        Usuario u5 = new Usuario("wasd", "ijkl");
+
+        usuarios.add(u1);
+        usuarios.add(u2);
+        usuarios.add(u3);
+        usuarios.add(u4);
+        usuarios.add(u5);
+
+        String ingresoU = tfusuario.getText();
+        String ingresoC = tfcontrasenia.getText();
+        boolean datosErroneos = true;
+
+        for (Usuario u : usuarios) {
+            if (u.getUsuario().equals(ingresoU) && u.getContrasenia().equals(ingresoC)) {
+                datosErroneos = false;
+            }
+        }
+        if (datosErroneos) {
+            Label lb = new Label("Datos Erróneos");
+            lb.setTextFill(Color.PALEVIOLETRED); // #ff2c56
+            lb.setFont(Font.font("System", FontWeight.BOLD, 15)); 
+            lb.setTextAlignment(TextAlignment.CENTER);
+
+            vLabelError.getChildren().add(lb);
+        }
     }
 }
