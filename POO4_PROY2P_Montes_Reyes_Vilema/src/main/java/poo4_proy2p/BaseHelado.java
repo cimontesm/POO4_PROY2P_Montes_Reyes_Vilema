d059045eca40146e42ecc2865cc2b3ae7ae1c65a
kiretigr <kiretigr@espol.edu.ec>
2023-09-03 13:19:42 -0500

    eliminar imports innecesarios

--- POO4_PROY2P_Montes_Reyes_Vilema/src/main/java/poo4_proy2p/BaseHelado.java
+++ POO4_PROY2P_Montes_Reyes_Vilema/src/main/java/poo4_proy2p/BaseHelado.java
@@ -4,20 +4,15 @@
  */
 package poo4_proy2p;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 
 /**
  *
