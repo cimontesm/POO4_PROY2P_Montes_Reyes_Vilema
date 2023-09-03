92aa6c52b5be7944a85270e36aa5dbe5cba68d09
cmontes <cmontes@190.155.147.164>
2023-09-03 15:53:42 -0500

    clase topping y un pequeño cambio en el fxml -montes

--- POO4_PROY2P_Montes_Reyes_Vilema/src/main/java/poo4_proy2p/Topping.java
+++ POO4_PROY2P_Montes_Reyes_Vilema/src/main/java/poo4_proy2p/Topping.java
@@ -4,6 +4,10 @@
  */
 package poo4_proy2p;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -17,47 +21,73 @@ import javafx.scene.layout.VBox;
  * @author cmontes
  */
 public class Topping {
+
     String nombre;
     double precio;
-    
+
     @FXML
     private Pane root;
-    
+
     @FXML
     private ImageView fondo;
-    
+
     @FXML
     private VBox vcb;
-    
+
     @FXML
     private CheckBox cb1;
-    
+
     @FXML
     private CheckBox cb2;
-    
+
     @FXML
     private CheckBox cb3;
-    
+
     @FXML
     private CheckBox cb4;
-    
+
     @FXML
     private CheckBox cb5;
-    
+
     @FXML
     private CheckBox cb6;
-    
+
     @FXML
     private Label lblValor;
-            
+
     @FXML
     private Button btnContinuar;
-    
+
     @FXML
-    public void continuar(){
-        
+    public void continuar() {
+
     }
-            
-            
-            
+
+    public void cargarTopping() {
+        try ( BufferedReader br = new BufferedReader(new FileReader("toppings.txt"))) {
+            String linea;
+            int i = 1;
+            while ((linea = br.readLine()) != null && i <= 6) {
+                String[] items = linea.split(",");
+                if (items.length >= 2) {
+                    String nombreTopping = items[0].trim();
+                    String precioTopping = items[1].trim();
+
+                    String checkBoxName = "cb" + i;
+                    CheckBox checkBox = (CheckBox) getClass().getDeclaredField(checkBoxName).get(this);
+                    checkBox.setText(nombreTopping + " - " + precioTopping);
+
+                    i++;
+                }
+
+            }
+
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException | NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
