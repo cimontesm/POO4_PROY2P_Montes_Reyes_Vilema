e403661b1732cf05d04cbac381b8ec335f3b2db8
Daniel <danielvilema@hotmail.com>
2023-09-03 20:08:56 -0500

    leer archivo de locales- metodo

--- POO4_PROY2P_Montes_Reyes_Vilema/src/main/java/poo4_proy2p/VentanaOpciones.java
+++ POO4_PROY2P_Montes_Reyes_Vilema/src/main/java/poo4_proy2p/VentanaOpciones.java
@@ -5,11 +5,14 @@
 package poo4_proy2p;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.ResourceBundle;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
@@ -19,10 +22,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -56,7 +63,6 @@ public class VentanaOpciones implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        hallarLocales();
         lblBienvenida.setText("Bienvenido/a "+usuario.getUsuario());
         Thread pedidos = new Thread(()->{
             while(true){
@@ -157,16 +163,55 @@ public class VentanaOpciones implements Initializable {
         }
     }
 
-    @FXML
-    private void mostrarMapa(ActionEvent event) {
+  
+    private void mostrarLocales(AnchorPane root) {
+        ArrayList<Local> locales = hallarLocales();
+        ArrayList<ImageView> ubicaciones = new ArrayList<>();
+        Thread hilo = new Thread(()->{
+            for(Local local:locales){
+                Platform.runLater(()->{
+                    try(FileInputStream input = new FileInputStream(new File("src/main/resources/poo4_proy2p/logo1.png"))){
+                        Image image = new Image(input);
+                        
+                        ImageView punto = new ImageView(image);
+                        punto.setFitWidth(300);
+                        punto.setFitHeight(300);
+                        punto.setLayoutX(local.getCoordX());
+                        punto.setLayoutY(local.getCoordY());
+                        if (!ubicaciones.contains(punto)) {
+                            ubicaciones.add(punto);
+                            root.getChildren().addAll(punto);
+                        }
+                        punto.setOnMouseClicked(event->mostrarInfo(local.nombre,local.horario));
+                        
+                        
+                        
+                        
+                    } catch (FileNotFoundException ex) {
+                        ex.printStackTrace();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                });
+                
+                int tiempo = new Random().nextInt(10)+1;
+                try{
+                    Thread.sleep(tiempo*1000);
+                }catch(InterruptedException e){
+                    System.out.println("Algo salio mal");
+                }
+            }
         
+        });
+        hilo.setDaemon(true);
+        hilo.start();
         
         
     }
     
     
     
-    public void hallarLocales(){
+    public ArrayList<Local> hallarLocales(){
         ArrayList<Local> locales = new ArrayList<>();
         try(BufferedReader bf = new BufferedReader(new FileReader("locales.txt"))){
             String linea = bf.readLine();
@@ -185,12 +230,75 @@ public class VentanaOpciones implements Initializable {
             
             
         } catch (FileNotFoundException ex) {
-            ex.printStackTrace();
+            System.out.println("No se encontro el archivo");;
         } catch (IOException ex) {
-            ex.printStackTrace();
+            System.out.println("Algo salio mal");
         }
+        
+        return locales;
 
     }
     
+    public void mostrarInfo(String nombre, String horario){
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Informacion de Local");
+        alert.setHeaderText(nombre);
+        //alert.setContentText("Horario: "+horario);
+        
+        Thread duracion = new Thread(()->{
+            int tiempo = 5; 
+            while(tiempo>0){
+                String mensaje = "Cerrando en "+tiempo+" segundos.";
+                Platform.runLater(()->{
+                    alert.setContentText("Horario: "+horario+"\n"+mensaje);
+
+                });
+                try{
+                    Thread.sleep(1000);
+                }catch(InterruptedException e){
+                    System.out.println("Algo salio mal");
+                }
+                tiempo --;
+               
+            }
+            Platform.runLater(()->{
+                
+                alert.close();
+        
+            });
+            
+        
+        });
+        
+        duracion.setDaemon(true);
+        duracion.start();
+        alert.showAndWait();
+        
+        
+    }
+
+    @FXML
+    private void verMapa(ActionEvent event) {
+            
+        Stage stage = new Stage();
+        AnchorPane root = new AnchorPane();
+        Scene scene = new Scene(root,800,600);
+        ImageView imageView = null;
+
+        try(FileInputStream input = new FileInputStream(new File("src/main/resources/poo4_proy2p/mapa2.png"))){
+            Image image = new Image(input,800,600,true,true);
+            imageView = new ImageView(image);
+            
+        } catch (FileNotFoundException ex) {
+            System.out.println("No se encontro el archivo");;
+        } catch (IOException ex) {
+            System.out.println("Algo salio mal");
+        }
+        
+        root.getChildren().add(imageView);
+        mostrarLocales(root);
+        stage.setScene(scene);
+        stage.show();
+    }
     
 }
