dcd0c784151b786e2f99e164d40bd74b68a7bd29
Daniel <danielvilema@hotmail.com>
2023-09-05 23:31:56 -0500

    javadoc pago

--- POO4_PROY2P_Montes_Reyes_Vilema/src/main/java/poo4_proy2p/Pago.java
+++ POO4_PROY2P_Montes_Reyes_Vilema/src/main/java/poo4_proy2p/Pago.java
@@ -25,44 +25,92 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.control.ToggleGroup;
+
 /**
+ * Clase pago Nos ayuda a generar las ventanas finales para saber cuánto debe
+ * pagar el usuario con efectivo o tarjeta.
  *
- * @author cmontes
+ * @author Cecilia Montes
+ * @author Kimberly Reyes
+ * @author Daniel Vilema
  */
 public class Pago implements Pagable, Initializable {
+
     private String nombre;
     private double total;
     private String tipo;
 
+    /**
+     * Construye un nuevo objeto Pago con la información proporcionada.
+     *
+     * @param nombre El nombre del beneficiario (String).
+     * @param total El monto total del pago (double).
+     * @param tipo El tipo de pago (String), por ejemplo, tarjeta de crédito o
+     * efectivo.
+     */
     public Pago(String nombre, double total, String tipo) {
         this.nombre = nombre;
         this.total = total;
         this.tipo = tipo;
     }
-    public Pago(){
-        
+
+    /**
+     * Construye un nuevo objeto Pago sin información inicial.
+     */
+    public Pago() {
+
     }
 
+    /**
+     * Obtiene el nombre del Usuario.
+     *
+     * @return El nombre del Usuario como una cadena de caracteres.
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Metodo setter para establecer el nombre del Usuario.
+     *
+     * @param nombre
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Obtiene el total del Usuario.
+     *
+     * @return El total del Usuario como un double.
+     */
     public double getTotal() {
         return total;
     }
 
+    /**
+     * Metodo setter para establecer el total del Usuario.
+     *
+     * @param total
+     */
     public void setTotal(double total) {
         this.total = total;
     }
 
+    /**
+     * Obtiene el tipo de Usuario.
+     *
+     * @return El tipo de Usuario como una cadena de caracteres.
+     */
     public String getTipo() {
         return tipo;
     }
 
+    /**
+     * Establece el tipo de Usuario.
+     *
+     * @param tipo El nuevo tipo de Usuario como una cadena de caracteres.
+     */
     public void setTipo(String tipo) {
         this.tipo = tipo;
     }
@@ -113,6 +161,13 @@ public class Pago implements Pagable, Initializable {
     @FXML
     private ToggleGroup modopago;
 
+    /**
+     * Este método se utiliza para confirmar una acción y mostrar una ventana
+     * final.
+     *
+     * @throws IOException Si ocurre un error al intentar mostrar la ventana
+     * final.
+     */
     @FXML
     public void confirmar() throws IOException {
         OrdenGenerada.mostrarVentanaFinal();
@@ -144,6 +199,11 @@ public class Pago implements Pagable, Initializable {
         }
     }
 
+    /**
+     * Genera una transacción de pago basada en la selección del modo de pago.
+     *
+     * @return Un objeto de tipo Pago que representa la transacción generada.
+     */
     @Override
     public Pago generarTransaccion() {
         Toggle seleccion = modopago.getSelectedToggle();
@@ -151,55 +211,64 @@ public class Pago implements Pagable, Initializable {
         double iva = 0.83;
         double adicionalT = 0.63;
         Pago p = null;
-        if (seleccion==rbtarjeta) {
-                //incrementar valor a pagar por 10% TO DO
-                total += total * 0.10;
+        if (seleccion == rbtarjeta) {
+            //incrementar valor a pagar por 10% TO DO
+            total += total * 0.10;
 
-                tfvalor.setText(String.valueOf(total));
-                tfvalor.setEditable(false);
-                tfadt.setText(String.valueOf(adicionalT));
-                tfiva.setText(String.valueOf(iva));
-                tftot.setText(String.valueOf(total+iva+adicionalT));
-                tfadt.setEditable(false);
-                tfiva.setEditable(false);
-                tftot.setEditable(false);
-                p = new Pago(VentanaOpciones.usuario.usuario,total+(iva*total)+(adicionalT*total),"Tarjeta");
-                
+            tfvalor.setText(String.valueOf(total));
+            tfvalor.setEditable(false);
+            tfadt.setText(String.valueOf(adicionalT));
+            tfiva.setText(String.valueOf(iva));
+            tftot.setText(String.valueOf(total + iva + adicionalT));
+            tfadt.setEditable(false);
+            tfiva.setEditable(false);
+            tftot.setEditable(false);
+            p = new Pago(VentanaOpciones.usuario.usuario, total + (iva * total) + (adicionalT * total), "Tarjeta");
 
-                try {
-                    mostrarVentanaTarjeta();
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
-        }   else if (seleccion==rbefectivo) {
-                tfvalor.setText(String.valueOf(total));
-                tfadt.setText("0.0");
-                tfiva.setText(String.valueOf("0.0"));
-                tftot.setText(String.valueOf(total+(iva*total)));
-                hdatos.getChildren().add(new Label("Acercarse a Caja para pagar tu pedido."));
-                
-                
-                
+            try {
+                mostrarVentanaTarjeta();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        } else if (seleccion == rbefectivo) {
+            tfvalor.setText(String.valueOf(total));
+            tfadt.setText("0.0");
+            tfiva.setText(String.valueOf("0.0"));
+            tftot.setText(String.valueOf(total + (iva * total)));
+            hdatos.getChildren().add(new Label("Acercarse a Caja para pagar tu pedido."));
 
-                tfvalor.setEditable(false);
-                tfadt.setEditable(false);
-                tfiva.setEditable(false);
-                tftot.setEditable(false);
-                p = new Pago(VentanaOpciones.usuario.usuario,total+(iva*total),"Efectivo");
-            }else{
+            tfvalor.setEditable(false);
+            tfadt.setEditable(false);
+            tfiva.setEditable(false);
+            tftot.setEditable(false);
+            p = new Pago(VentanaOpciones.usuario.usuario, total + (iva * total), "Efectivo");
+        } else {
             tfvalor.setText("Hola");
         }
         return p;
     }
-    
-    public double obtenerSuma(){
+
+    /**
+     * Calcula la suma total de los valores a pagar almacenados en la lista
+     * "valoresAPagar".
+     *
+     * @return La suma total de los valores a pagar como un número de punto
+     * flotante (double).
+     */
+    public double obtenerSuma() {
         double total = 0;
-        for(double valor:VentanaOpciones.valoresAPagar){
-            total+=valor;
+        for (double valor : VentanaOpciones.valoresAPagar) {
+            total += valor;
         }
         return total;
     }
 
+    /**
+     * Muestra la ventana de pago para realizar una transacción de pago.
+     *
+     * @throws IOException Si ocurre un error al cargar la interfaz gráfica de
+     * la ventana de pago.
+     */
     public static void mostrarVentanaPago() throws IOException {
         FXMLLoader fxmLoader = new FXMLLoader(Pedido.class.getResource("pago.fxml"));
         Pago.root = fxmLoader.load();
@@ -210,8 +279,14 @@ public class Pago implements Pagable, Initializable {
 
     }
 
+    /**
+     * Muestra una ventana para ingresar los datos de una tarjeta de crédito.
+     *
+     * @throws IOException Si ocurre un error al cargar la interfaz gráfica de
+     * la ventana de tarjeta.
+     */
     @FXML
-    public  void mostrarVentanaTarjeta() throws IOException {
+    public void mostrarVentanaTarjeta() throws IOException {
         FXMLLoader fxmLoader = new FXMLLoader(Pedido.class.getResource("pago.fxml"));
         VBox vb = new VBox();
         HBox hb = new HBox();
@@ -223,7 +298,6 @@ public class Pago implements Pagable, Initializable {
         Label cvv = new Label("CVV: ");
         vlab.getChildren().addAll(nom, num, caducidad, cvv);
         vlab.setSpacing(24);
-        
 
         VBox vdatos = new VBox();
         TextField tfnombre = new TextField();
@@ -232,24 +306,20 @@ public class Pago implements Pagable, Initializable {
         TextField tfcvv = new TextField();
         vdatos.getChildren().addAll(tfnombre, tfnum, tfcaduc, tfcvv);
         vdatos.setSpacing(15);
-        
 
         Label l = new Label("Ingrese los datos de su tarjeta:");
         l.setStyle("-fx-font-weight: bold; -fx-font-size: 14px;");
         Color colorP = Color.web("#3e0b63");
         l.setTextFill(colorP);
-        
-        
-        
-        
+
         root.getChildren().add(l);
         l.setLayoutX(293);
         l.setLayoutY(151);
 
-        hb.getChildren().addAll( vlab, vdatos);
+        hb.getChildren().addAll(vlab, vdatos);
         hb.setSpacing(20);
-        HBox.setMargin(hb, new Insets(10,0,0,5));
-        
+        HBox.setMargin(hb, new Insets(10, 0, 0, 5));
+
         hdetalle.getChildren().add(hb);
         hdetalle.setSpacing(20);
         BackgroundFill backgroundFill = new BackgroundFill(Color.PINK, null, null);
@@ -257,6 +327,14 @@ public class Pago implements Pagable, Initializable {
         hdetalle.setBackground(background);
     }
 
+    /**
+     * Inicializa la ventana o control y realiza acciones de inicialización,
+     * como generar una transacción.
+     *
+     * @param url La ubicación relativa de la raíz del objeto a inicializar.
+     * @param rb El recurso de recursos que se pasa a través de la
+     * inicialización, o nulo si no hay ninguno.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         generarTransaccion();
